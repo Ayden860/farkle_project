dec728e322557ed567361ea58355c03bad8d9187
yang_1014201 <1014201@my.scccd.edu>
2025-11-26 01:30:03 -0800

    die.cc's weight roll

--- die.cc
+++ die.cc
@@ -25,6 +25,19 @@ int Die::get_roll() {
 }
 
 void Die::roll() {
-	//TODO: Implement weight system
-	last_roll = rand() % SIDES + 1;
+    //TODO: Implement weight system
+    //range of 0-1
+    double ran_num = (double)rand() / RAND_MAX;
+    double total = 0.0;
+    //new weight plus all new weights divide by total weights must be 1, but to find the "weight" of one side its that weight divide by total weight
+    //get weight vector per side
+    for(size_t i = 0; i < weight.size(); i++){
+        total += weight[i];
+        if(ran_num < total){
+            last_roll = i + 1;//range(1, SIDES)
+            return;
+        }
+    }
+    //range(1, SIDES)
+    //last_roll = rand() % SIDES + 1;
 }
